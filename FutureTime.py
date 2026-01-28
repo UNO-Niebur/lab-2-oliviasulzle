fd803f1021e6ca98cdb3ddb663a3c90ab4bdf49e
oliviasulzle <94474097@nebraska.edu>
2026-01-28 17:29:21 +0000

    Lab 2 Completed

--- FutureTime.py
+++ FutureTime.py
@@ -1,7 +1,7 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
+#Name: Olivia Sulzle
+#Date: 2/1/26
+#Assignment: Lab 2
 
 # datetime will allow us to access the system date and time.
 import datetime
@@ -12,17 +12,25 @@ def main():
   currentHour = now.hour
   currentMinute = now.minute
 
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
+  hours_to_add = int(input("Enter hours to add: "))
   #Ask user for minutes
+  minutes_to_add = int(input("Enter minutes to add: "))
 
   #Calculate the time after the user-supplied time has passed.
+  total_minutes = currentMinute + minutes_to_add
+  final_minute = total_minutes % 60
+  extra_hour = total_minutes // 60
+
+  total_hours = currentHour + hours_to_add + extra_hour
+  final_hour = total_hours % 24
 
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
+  print(f"Future time: {final_hour:02}:{final_minute:02}")
 
 
 if __name__ == '__main__':
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,7 +1,7 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Olivia Sulzle
+#Date: 2/1/26
+#Assignment: Lab 2
 
 #We will need random for this program, import to use this package.
 import random
@@ -9,9 +9,12 @@ import random
 def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
+  responses = ["It is certain", "Yes definitely", "Without a doubt", "Ask again later", "Cannot predict now", "Sources say no", "Very doubtful", "Don't count on it"]
   #Prompt the user for their question.
+  input("What is your question? ")
 
   #Answer question randomly with one of the options from your earlier list.
+  print(random.choice(responses))
 
 
 if __name__ == '__main__':
